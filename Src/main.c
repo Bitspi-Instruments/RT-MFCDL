717cf4fbbdd3a746fdc7ee58df2b697a7527e3d6
kpeeyush <peeyush.ei@gmail.com>
2016-02-03 18:51:20 +0530

    added PC2 blinky

--- Src/main.c
+++ Src/main.c
@@ -85,6 +85,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_2);
+		HAL_Delay(100);
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
